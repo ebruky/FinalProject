6af8c724f65fb252b77eb892ebc6f43efc248a62
ebruky <ebruky.2525@gmail.com>
2021-02-16 12:10:19 +0300

    webapi

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,67 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]            //attribute
+    public class ProductsController : ControllerBase
+    {
+
+
+        //IoC Container --- Inversion of    Control  ,,  startup ta ConfigureServices metotunu kullan 
+
+
+
+        IProductService _productService;
+
+        public ProductsController(IProductService ıproductService)
+        {
+            _productService = ıproductService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            
+            var result =_productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(Product product)
+        {
+
+            var result = _productService.Add(product);
+            if (result.Success)
+                {
+                    return Ok(result);
+                }
+            return BadRequest(result);
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+    }
+}
+
