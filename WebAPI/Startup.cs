960cc209ce8b304d8b2fc8019d03a6bee77cbbde
ebruky <ebruky.2525@gmail.com>
2021-04-02 02:15:13 +0300

    .

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -76,6 +76,10 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.ConfigureCustomExceptionMiddleware();   
+
+
             app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
             app.UseHttpsRedirection();
 
